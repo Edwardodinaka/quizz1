d7a420bb9eb12e3f50f46387b034e55d589a41ff
Edward <edwardodinaka@gmail.com>
2022-10-06 09:43:43 +0100

    quizz app

---
+++ src/components/data.js
@@ -0,0 +1,185 @@
+const data = [
+    {
+        id : 1,
+        question : "Computer is a what ....",
+        answer : [
+            {
+                text : "Electronic",
+                correct: true
+            },
+
+            {
+                text : "Manual",
+                correct : false
+            },
+
+            {
+                text : "Typewriter",
+                correct : false
+            },
+
+            {
+                text : "coding",
+                correct : false
+            },
+        ]
+    },
+
+    {    
+    id : 2,
+        question : "A company that has offered shares to the people, public and has limited liabilities is what ...",
+        answer : [
+            {
+                text : "Private",
+                correct: false
+            },
+
+            {
+                text : "Co-Operative",
+                correct : false
+            },
+
+            {
+                text : "Public",
+                correct : true
+            },
+
+            {
+                text : "Society",
+                correct : false
+            },
+        ]
+    },
+
+    {    
+        id : 3,
+            question : "NAFDAC was formed in what year",
+            answer : [
+                {
+                    text : "1936",
+                    correct: false
+                },
+    
+                {
+                    text : "1851",
+                    correct : false
+                },
+    
+                {
+                    text : "1966",
+                    correct : false
+                },
+    
+                {
+                    text : "1988",
+                    correct : true
+                },
+            ]
+        },
+
+        {    
+            id : 4,
+                question : "Auguste Comte was credited to be the most influential philosophers of the early times, he was born in what year",
+                answer : [
+                    {
+                        text : "1789",
+                        correct: false
+                    },
+        
+                    {
+                        text : "1841",
+                        correct : false
+                    },
+        
+                    {
+                        text :"1789",
+                        correct : true
+                    },
+        
+                    {
+                        text : "1960",
+                        correct : false
+                    },
+                ]
+            },
+
+            {    
+                id : 5,
+                    question : "Herbert Spencer was born in what...",
+                    answer : [
+                        {
+                            text : "1967",
+                            correct: false
+                        },
+            
+                        {
+                            text : "1903",
+                            correct : true
+                        },
+            
+                        {
+                            text : "1666",
+                            correct : false
+                        },
+            
+                        {
+                            text : "1989",
+                            correct : false
+                        },
+                    ]
+                },
+
+                {    
+                    id : 6,
+                        question : "Since after the britian left Nigeria, what year did they get independence ",
+                        answer : [
+                            {
+                                text :"1989",
+                                correct: false
+                            },
+                
+                            {
+                                text : "1960",
+                                correct : true
+                            },
+                
+                            {
+                                text : "1966",
+                                correct : false
+                            },
+                
+                            {
+                                text : "1989",
+                                correct : false
+                            },
+                        ]
+                    },
+
+                    {    
+                        id : 7,
+                            question : "The Rivers State house of Assembly passed a law of secrets cult and similar activities (prohibition) law in waht year",
+                            answer : [
+                                {
+                                    text : "1966",
+                                    correct: false
+                                },
+                    
+                                {
+                                    text : "2012",
+                                    correct : true
+                                },
+                    
+                                {
+                                    text : "2002",
+                                    correct : false
+                                },
+                    
+                                {
+                                    text : "2004",
+                                    correct : false
+                                },
+                            ]
+                        },
+]
+
+export default data;
